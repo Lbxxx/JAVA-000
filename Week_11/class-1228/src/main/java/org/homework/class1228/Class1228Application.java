81b458dc43778617456502a0b0540a8085eeff0a
linboxuan <linboxuan001@ke.com>
2021-01-07 00:47:47 +0800

    add homework11

---
+++ Week_11/class-1228/src/main/java/org/homework/class1228/Class1228Application.java
@@ -0,0 +1,13 @@
+package org.homework.class1228;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Class1228Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Class1228Application.class, args);
+    }
+
+}
---
+++ Week_11/class-1228/src/test/java/org/homework/class1228/Class1228ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.homework.class1228;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Class1228ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
